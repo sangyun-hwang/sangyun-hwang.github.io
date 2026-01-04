e1690f75ff8030ea404cb39d3fc9f31d125ba061
황상윤 <s_infinite@naver.com>
2026-01-05 00:05:26 +0900

    [init] gh-pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: 'export',
+  reactStrictMode: true,
+  images: {
+    unoptimized: true
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
