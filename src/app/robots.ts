a0b26698c24f542b903f3e9ddce142e07c262b13
황상윤 <s_infinite@naver.com>
2026-01-10 05:39:03 +0900

    [fix] robots.ts 수정

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,16 @@
 import type { MetadataRoute } from "next";
 import { BASE_URL } from "@/shared/config/constant";
 
+const BASE_URL = "https://sangyun-hwang.github.io/";
+
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
     sitemap: `${BASE_URL}/sitemap.xml`,
   };
 }
