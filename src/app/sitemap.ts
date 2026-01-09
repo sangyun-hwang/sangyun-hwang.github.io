820c7ffe381c9d0dbb8bb02741059ab16e5e4b5d
황상윤 <s_infinite@naver.com>
2026-01-10 05:30:14 +0900

    [feat] sitemap.ts 추가

---
+++ src/app/sitemap.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from "next";
+import { getAllPosts, getCategories } from "@/entities/post/model/post";
+import { BASE_URL } from "@/shared/config/constant";
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const baseUrl = BASE_URL;
+  const categories = await getCategories();
+  const categoriesUrls = categories.map((category) => ({
+    url: `${baseUrl}/${category}`,
+    lastModified: new Date(),
+  }));
+
+  const posts = await getAllPosts();
+  const postUrls = posts.map((post) => ({
+    url: `${baseUrl}/${post.filePath.join("/")}`,
+    lastModified: post.releaseDate,
+  }));
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+    },
+    ...categoriesUrls,
+    ...postUrls,
+  ];
+}
