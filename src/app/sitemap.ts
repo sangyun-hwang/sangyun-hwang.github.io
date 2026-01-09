df93f77ee8e2952c1f9f1894d569a964ec6bfb53
황상윤 <s_infinite@naver.com>
2026-01-10 05:44:22 +0900

    [fix] sitemap.ts 수정

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,9 @@
 import type { MetadataRoute } from "next";
 import { getAllPosts, getCategories } from "@/entities/post/model/post";
 import { BASE_URL } from "@/shared/config/constant";
+
+export const dynamic = "force-static";
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = BASE_URL;
   const categories = await getCategories();
