aaa98018d237f2f47cda976a9039010dab97f9ae
황상윤 <s_infinite@naver.com>
2026-01-10 05:42:23 +0900

    [fix] robots.ts 수정

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,5 +1,7 @@
 import type { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 const BASE_URL = "https://sangyun-hwang.github.io/";
 
 export default function robots(): MetadataRoute.Robots {
