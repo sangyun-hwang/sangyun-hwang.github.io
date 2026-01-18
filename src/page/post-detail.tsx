9679b15af24b4d58b2cbab99dcfce1732a2fbfb3
황상윤 <s_infinite@naver.com>
2026-01-18 22:30:46 +0900

    [feat] breadcrumb ui 추가

--- src/page/post-detail.tsx
+++ src/page/post-detail.tsx
@@ -1,6 +1,7 @@
 import type { PostWithFrontmatterType } from "@/entities/post/model/post.type";
 import { MdxRemote } from "@/entities/post/ui/mdx/mdx-remote";
 import { PostTitle } from "@/entities/post/ui/post/post-title";
+import { Breadcrumb } from "@/shared/common-ui/breadcrumb";
 import { cn } from "@/shared/utils/cn";
 
 type PostPageProps = {
@@ -10,7 +11,9 @@ type PostPageProps = {
 export default function PostDetailPage({ post }: PostPageProps) {
   return (
     <section className={cn("md:max-w-3xl")}>
+      <Breadcrumb postTitle={post.title} />
       <PostTitle>{post.title}</PostTitle>
+      <br />
       <MdxRemote source={post.content} />
     </section>
   );
