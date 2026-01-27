396df9497d4d700c369e6c8282315e3f4aef983d
황상윤 <s_infinite@naver.com>
2026-01-27 23:06:04 +0900

    [style] posts page 제목 추가

--- src/page/posts.tsx
+++ src/page/posts.tsx
@@ -1,5 +1,6 @@
 import type { PostWithFrontmatterType } from "@/entities/post/model/post.type";
 import { PostCard } from "@/entities/post/ui/post/post-card";
+import { PostTitle } from "@/entities/post/ui/post/post-title";
 import { Breadcrumb } from "@/shared/common-ui/breadcrumb";
 import { cn } from "@/shared/utils/cn";
 
@@ -11,6 +12,7 @@ const PostPage = ({ posts }: PostPageProps) => {
   return (
     <main className={cn("md:max-w-3xl md:mx-auto")}>
       <Breadcrumb />
+      <PostTitle title="황상윤 블로그의 모든 글" />
       <br />
       <ul className={cn("list-none p-0")}>
         {posts.map((post) => (
